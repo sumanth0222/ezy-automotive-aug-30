b26c6834e7790bde7e23a091cc52a420a3a83d77
Desaji <desaji@infinitechsoft.com>
2024-10-22 17:09:43 +0530

    Popup in transaction page and systemconfiguration pages created

---
+++ src/components/reports/partsreports/manageConsumbleBrands.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ManageConsumbleBrands = ()=>{
+    return(
+
+        <h1>This is Manage Consumble Brands page From System Configuration</h1>
+    )
+}
+export default ManageConsumbleBrands;
